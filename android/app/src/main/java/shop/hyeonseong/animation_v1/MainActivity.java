ec5306b0608f373a106e9ce516172d4f656e4d38
seong9566 <dlgustjd9566@gmail.com>
2023-09-07 15:57:43 +0900

    기본 셋팅 완료

---
+++ android/app/src/main/java/shop/hyeonseong/animation_v1/MainActivity.java
@@ -0,0 +1,6 @@
+package shop.hyeonseong.animation_v1;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AnimationV1());
+}
+
+class AnimationV1 extends StatelessWidget {
+  const AnimationV1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
